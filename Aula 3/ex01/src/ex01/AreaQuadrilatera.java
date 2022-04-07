abf06bb2dde8428611d7e3512b7ded3558d95e04
Rafael Soares <89417905+RafaelHGS@users.noreply.github.com>
2022-04-07 09:12:45 -0300

    Add files via upload

---
+++ Aula 3/ex01/src/ex01/AreaQuadrilatera.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex01;
+
+/**
+ *
+ * @author 125111374176
+ */
+public class AreaQuadrilatera {
+
+   public int calcularArea(int lado){
+       return lado * lado;
+   }
+   
+   public int calcularArea(int base, int altura){
+       return base * altura;
+   }
+}
---
+++ Aula 3/ex01/src/ex01/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex01;
+
+public class Main {
+    public static void main(String[] args) {
+        AreaQuadrilatera quadrado = new AreaQuadrilatera();
+        
+        System.out.println(quadrado.calcularArea(2));
+        System.out.println(quadrado.calcularArea(3,7));
+        
+    }
+}
