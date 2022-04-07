9b82bc8ce6b596642516a513f3fb1d3399f39aec
Rafael Soares <89417905+RafaelHGS@users.noreply.github.com>
2022-04-07 11:15:10 -0300

    Add files via upload

---
+++ Aula3/Ex02/Calculadora.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex02;
+
+/**
+ *
+ * @author 125111374176
+ */
+public class Calculadora {
+    public double operacao(double n1, double n2){
+        return n1 + n2;
+    }
+    
+    public double operacao(int n1, double n2){
+        return n1 - n2;
+    }
+    
+    public double operacao(double n1, int n2){
+        return n1 * n2;
+    }
+    
+    public double operacao(int n1, int n2){
+        return n1 / n2;
+    }
+    
+    public double operacao(int n1, short n2){
+        return n1 % n2;
+    }
+    
+}
